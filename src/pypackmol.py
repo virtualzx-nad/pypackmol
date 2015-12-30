e6d9191da2814e2a9e3bf2860c3b9c2b8db54b4d
Xiaolei Zhu <virtualzx@gmail.com>
2015-12-30 11:35:05 -0800

    Finished most functionalities.  No initial testing yet.

--- src/pypackmol.py
+++ src/pypackmol.py
@@ -16,6 +16,7 @@ __status__    = "Prototype"
 
 import tempfile
 import os
+import subprocess
 import numpy as np
 
 # If openbabel is imported, it is possible to convert different file types,
@@ -28,16 +29,13 @@ try :
 except :
     _has_openbabel=False
 
-PACKMOL_FORMATS = ['xyz','pdb','tinker','moldy']
 
-
-def convert_to_xyz(input_data,input_format='auto') :
+def convert_to_xyz(input_data,input_format='auto',force_field='uff') :
     """Convert input format unrecognized by packmol into xyz files.
     
-    If the input molecular structure data can be recognized by packmol, the 
-    function simply transfer the data to a temp file.  When the input is not
-    recognized by packmol, the function use OpenBabel to convert into xyz 
-    format, then save this to a temp file.
+    If the input molecular structure data is in a xyz file, the function 
+    simply transfer the data to a temp file. Otherwise, the function use 
+    OpenBabel to convert into xyz format, then save this to a temp file.
     
     Args:
         input_data: Filename or SMILES string to be converted
@@ -48,11 +46,9 @@ def convert_to_xyz(input_data,input_format='auto') :
             be a SMILES string. 
     
     Returns:
-        A named temporary file containing the molecular structure in a packmol 
-        recognized format (xyz, pdb, tinker or moldy).  When conversion is 
-        needed, the function always convert to xyz.  Note that since this is a 
-        temp file, it will automatically be deleted when the file object is 
-        unreferenced or destroyed.
+        A named temporary file containing the molecular structure in xyz format.  
+        Note that since this is a temp file, it will automatically be deleted 
+        when the file object is unreferenced or destroyed.
     """
     input_format=input_format.lower()
     if input_format == 'auto' :
@@ -62,22 +58,22 @@ def convert_to_xyz(input_data,input_format='auto') :
         except :
             input_format ='smi'
             
-    # If the file is packmol recognizable, there is no need to convert
-    if input_format in PACKMOL_FORMATS :
+    # Create an xyz temp file and perform conversion
+    output_file = tempfile.NamedTemporaryFile(mode='w+',suffix='xyz')            
+    # If the file is xyz, there is no need to convert
+    if input_format == 'xyz' :
         with open(input_data) as in_file :
-            output_file = tempfile.NamedTemporaryFile(mode='w+',suffix=input_format)
             output_file.write(in_file.read())
     else :
-        # The file is packmol compatible format.  Conversion to xyz is needed. 
+        # Conversion to xyz is needed. 
         # If do not have openbabel, then we are pretty screwed here.
         if not _has_openbabel :
             raise ImportWarning["Can't import OpenBabel for format conversion. " +
                        "Please use xyz format instead."]
-        # Create an xyz temp file and perform conversion
-        output_file = tempfile.NamedTemporaryFile(mode='w+',suffix='xyz')
+        # Construct a openbabel `pybel.Molecule` object then export to xyz
         if input_format=='smi' or imput_format=='smiles' :
             mol=pb.readstring(input_format,input_data)
-            mol.make3D()
+            mol.make3D(forcefield=force_field,steps=200)
         else :
             mol=pb.readfile(input_format,input_data)
         output_file.write(mol.write('xyz'))
@@ -92,17 +88,46 @@ class Packmol (object):
     """Wrapper class for the packmol molecule packer program
     """
         
-    def __init__(self, region_type='sphere',  dimensions=10.0 , 
-                 tolerance=3.0 ,  **kwargs ):
+    def __init__(self, command_line='packmol', region_type='sphere', 
+                 dimensions=10.0, tolerance=3.0 , output='packed.xyz', 
+                 nloop=200, opt_force_field='uff',  **kwargs ):
         """Construct packmol run by providing simulation parameters.  
        
         """
         self._dimensions  = dimensions
         self._tolerance   = tolerance
-        self._region_type = region_type
+        self._nloop       = nloop
+        self._region_type = region_type.lower()
+        self._opt_force_field   = opt_force_field
         self._packmol_arguments = kwargs
+        self._command_line = command_line
         self._structure_list = []
 
+    def _prepare_input_file (self) :
+        """Create temp file to be used as packmol input file"""
+        self._packmol_input = tempfile.NamedTemporaryFile(mode="w+",suffix="inp")
+        self._packmol_input.write('tolerance {}\n'.format(self._tolerance))
+        self._packmol_input.write('nloop {}\n'.format(self._nloop))
+        self._packmol_input.write('filetype xyz\n')
+        self._packmol_input.write('output {}\n'.format(self._output))
+        # Write down other arguments 
+        for key,value in self._packmol_arguments.items():
+            self._packmol_input.write('{0} {1}\n'.format(key,value))
+        # 
+        for mol_type in self._structure_list :
+            self._packmol_input.write('structure {}\n'.format(mol_type['structure'].name))
+            self._packmol_input.write('  number {}\n'.format(mol_type['number']))
+            if self._region_type == 'sphere' :
+                self._packmol_input.write(
+                    '  inside sphere 0.0 0.0 0.0 {}\n'.format(self._dimensions))
+            elif self._region_type :
+                raise NotImplementedError('Currently only sphere regions are supported.')
+            self._packmol_input.write('  number {}\n'.format(mol_type['number']))
+            self._packmol_input.write('end structure\n')
+        self._packmol_input.flush()
+        self._packmol_input.seek(0)
+        return self._packmol_input
+    
     def add_structure(self, input_data, input_format='auto', count=1) :
         """Add one or more structure group to be packed.
         
@@ -111,12 +136,21 @@ class Packmol (object):
                 string that contains filename that contains this 
                 structure, or a SMILES string.  
         """
-        self._structure_list.append({ "structure": convert_to_xyz(input_data) ,
-                         "number" : count })
+        self._structure_list.append({ 
+                "structure": convert_to_xyz(input_data,input_format,self._opt_force_field) ,
+                "number"   : count })
 
-    def pack(self) :
-        pass
-        #Not implemented yet
+    def pack(self, print_output=False) :
+        outfile = tempfile.NamedTemporaryFile(mode='w+')
+        
+        code = subprocess.call(self._command_line, stdin=_prepare_input_file(),
+                        stdout=outfile )
+        outfile.seek(0)
+        packmol_output=outfile.read()
+        if print_output :
+            sys.stdout.write(packmol_output)
+        if code or 'ERROR' in packmol_output or 'STOP' in packmol_output :
+            raise Exception('Packing failed. exit code={}'.format(code))
 
     def autosize(self) :
         pass
