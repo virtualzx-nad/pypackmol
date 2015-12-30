3b110c600d7d1c13034a98713fc0abef6accb3a4
Xiaolei Zhu <virtualzx@gmail.com>
2015-12-30 15:03:39 -0800

    First complete working version that passed tests :  construction, packing, changing options and output

--- src/pypackmol.py
+++ src/pypackmol.py
@@ -11,7 +11,7 @@ __author__    = "Xiaolei Zhu"
 __copyright__ = "Martinez Group, Stanford University, CA, USA, Planet Earth"
 __maintainer__= "Xiaolei Zhu"
 __email__     = "virtualzx@gmail.com"
-__version__   = "0.0.1"
+__version__   = "0.1.0"
 __status__    = "Prototype"
 
 import tempfile
@@ -30,7 +30,7 @@ except :
     _has_openbabel=False
 
 
-def convert_to_xyz(input_data,input_format='auto',force_field='uff') :
+def convert_to_xyz(input_data,input_format="auto",force_field="uff") :
     """Convert input format unrecognized by packmol into xyz files.
     
     If the input molecular structure data is in a xyz file, the function 
@@ -39,11 +39,13 @@ def convert_to_xyz(input_data,input_format='auto',force_field='uff') :
     
     Args:
         input_data: Filename or SMILES string to be converted
-        input_format: Format of the input.  When the value is 'auto' which is 
+        input_format: Format of the input.  When the value is "auto" which is 
             the default, the function will try to determine the type of input 
             file. In such event, if the input string does not correspond to an 
             existing file that can be opened, the string is instead taken to 
             be a SMILES string. 
+        force_field: The force-field used by OpenBabel during constructions
+            of 3D structure if only 2D structures are available (i.e., SMILES)
     
     Returns:
         A named temporary file containing the molecular structure in xyz format.  
@@ -51,32 +53,32 @@ def convert_to_xyz(input_data,input_format='auto',force_field='uff') :
         when the file object is unreferenced or destroyed.
     """
     input_format=input_format.lower()
-    if input_format == 'auto' :
+    if input_format == "auto" :
         try :
             with open(input_data) as input_file :
-                input_format = input_data.split('.')[-1]
+                input_format = input_data.split(".")[-1]
         except :
-            input_format ='smi'
+            input_format ="smi"
             
     # Create an xyz temp file and perform conversion
-    output_file = tempfile.NamedTemporaryFile(mode='w+',suffix='xyz')            
+    output_file = tempfile.NamedTemporaryFile(mode="w+",suffix="xyz")            
     # If the file is xyz, there is no need to convert
-    if input_format == 'xyz' :
+    if input_format == "xyz" :
         with open(input_data) as in_file :
             output_file.write(in_file.read())
     else :
         # Conversion to xyz is needed. 
         # If do not have openbabel, then we are pretty screwed here.
         if not _has_openbabel :
-            raise ImportWarning["Can't import OpenBabel for format conversion. " +
+            raise ImportWarning["Cannot import OpenBabel for format conversion. " +
                        "Please use xyz format instead."]
         # Construct a openbabel `pybel.Molecule` object then export to xyz
-        if input_format=='smi' or imput_format=='smiles' :
+        if input_format=="smi" or imput_format=="smiles" :
             mol=pb.readstring(input_format,input_data)
             mol.make3D(forcefield=force_field,steps=200)
         else :
             mol=pb.readfile(input_format,input_data)
-        output_file.write(mol.write('xyz'))
+        output_file.write(mol.write("xyz"))
     
     output_file.flush()
     output_file.seek(0)
@@ -87,76 +89,92 @@ def convert_to_xyz(input_data,input_format='auto',force_field='uff') :
 class Packmol (object):
     """Wrapper class for the packmol molecule packer program
     """
-        
-    def __init__(self, command_line='packmol', region_type='sphere', 
-                 dimensions=10.0, tolerance=3.0 , output='packed.xyz', 
-                 nloop=200, opt_force_field='uff',  **kwargs ):
-        """Construct packmol run by providing simulation parameters.  
-       
-        """
-        self._dimensions  = dimensions
-        self._tolerance   = tolerance
-        self._nloop       = nloop
-        self._region_type = region_type.lower()
-        self._opt_force_field   = opt_force_field
-        self._packmol_arguments = kwargs
-        self._command_line = command_line
+    
+    # this is a list of options used by Packmol class but not the packmol program
+    NONPACKMOL_OPTIONS=["command_line", "region_type","dimension","opt_force_field"]
+    
+    def __init__(self, **kwargs ):
+        """Construct packmol run by providing simulation parameters."""
         self._structure_list = []
+        # default options
+        self._options = {"command_line":"packmol", "region_type":"sphere",
+                                "dimension":10, "tolerance":3.0, "output":"packed.xyz",
+                                "nloop":200, "opt_force_field":"uff"}       
+        self.set_options(**kwargs)
+        
+        
+    def set_options(self, **kwargs ):
+        """Update packmol option values.  """
+        for key,value in kwargs.items():
+            self._options[key] = value
+
 
     def _prepare_input_file (self) :
         """Create temp file to be used as packmol input file"""
-        self._packmol_input = tempfile.NamedTemporaryFile(mode="w+",suffix="inp")
-        self._packmol_input.write('tolerance {}\n'.format(self._tolerance))
-        self._packmol_input.write('nloop {}\n'.format(self._nloop))
-        self._packmol_input.write('filetype xyz\n')
-        self._packmol_input.write('output {}\n'.format(self._output))
+        inp_file = tempfile.NamedTemporaryFile(mode="w+",suffix="inp")
+        inp_file.write("filetype xyz\n")
         # Write down other arguments 
-        for key,value in self._packmol_arguments.items():
-            self._packmol_input.write('{0} {1}\n'.format(key,value))
+        for key,value in self._options.items():
+            if key not in Packmol.NONPACKMOL_OPTIONS:
+                inp_file.write("{0} {1}\n".format(key,value))
         # 
         for mol_type in self._structure_list :
-            self._packmol_input.write('structure {}\n'.format(mol_type['structure'].name))
-            self._packmol_input.write('  number {}\n'.format(mol_type['number']))
-            if self._region_type == 'sphere' :
-                self._packmol_input.write(
-                    '  inside sphere 0.0 0.0 0.0 {}\n'.format(self._dimensions))
-            elif self._region_type :
-                raise NotImplementedError('Currently only sphere regions are supported.')
-            self._packmol_input.write('  number {}\n'.format(mol_type['number']))
-            self._packmol_input.write('end structure\n')
-        self._packmol_input.flush()
-        self._packmol_input.seek(0)
-        return self._packmol_input
+            inp_file.write("structure {}\n".format(mol_type["structure"].name))
+            inp_file.write("  number {}\n".format(mol_type["number"]))
+            if self._options["region_type"] == "sphere" :
+                inp_file.write(
+                    "  inside sphere 0.0 0.0 0.0 {}\n".format(self._options["dimension"]))
+            elif self._options["region_type"] :
+                raise NotImplementedError("Currently only sphere regions are supported.")
+            inp_file.write("end structure\n")
+        inp_file.flush()
+        inp_file.seek(0)
+        self._input_stash=inp_file.read()
+        inp_file.seek(0)
+        return inp_file
     
-    def add_structure(self, input_data, input_format='auto', count=1) :
-        """Add one or more structure group to be packed.
+    def add_structure(self, input_data, input_format="auto", count=1) :
+        """Add one or more structure group to be packed by packmol.
         
         Args:
             input_data: molecular structure of the new group(s).  A 
                 string that contains filename that contains this 
                 structure, or a SMILES string.  
+            input_format: Format of `input_data`.  Can be any OpenBabel recognized file
+                format or `smi` for a SMILES.  When set to `auto`, which is the default,
+                the format will be determined from filename, or set to SMILES if it does
+                not correspond to any existing file.
+            count: Number of this structure to be packed.  Default is 1.
+            
         """
         self._structure_list.append({ 
-                "structure": convert_to_xyz(input_data,input_format,self._opt_force_field) ,
+                "structure": convert_to_xyz(input_data,input_format,
+                                            self._options["opt_force_field"]) ,
                 "number"   : count })
 
-    def pack(self, print_output=False) :
-        outfile = tempfile.NamedTemporaryFile(mode='w+')
-        
-        code = subprocess.call(self._command_line, stdin=_prepare_input_file(),
+    def pack(self, output=None) :
+        if output :
+            self._options["output"] = output
+        outfile = tempfile.NamedTemporaryFile(mode="w+")
+        code = subprocess.call(self._options["command_line"], stdin=self._prepare_input_file(),
                         stdout=outfile )
         outfile.seek(0)
-        packmol_output=outfile.read()
-        if print_output :
-            sys.stdout.write(packmol_output)
-        if code or 'ERROR' in packmol_output or 'STOP' in packmol_output :
-            raise Exception('Packing failed. exit code={}'.format(code))
+        packmol_output = outfile.read()
+        if code :
+            raise Exception("Packmol error termination. exit code={}".format(code))
+        if "ERROR" in packmol_output :
+            raise Exception("Individual molecules cannot be staged in packing region. " + 
+                            "Increase tolerance value.")
+        if "STOP" in packmol_output :
+            raise Exception(("Packing failed.  Cannot fulfill tolerance constraints. " +
+                           "Best result found in [{}]").format(self._options["output"]))
+        return {"filename":self._options["output"],"packmol output":packmol_output}
 
     def autosize(self) :
         pass
         #Not implemented yet
         
 # When called as standalone, perform a doctest run
-if __name__ == __main__ :
+if __name__ == "__main__" :
     import doctest
     doctest.testmod()
